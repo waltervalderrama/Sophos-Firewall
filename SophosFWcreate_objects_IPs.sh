079f259ae4ced20b4f6904a33ab7503416991b86
waltervalderrama <64512269+waltervalderrama@users.noreply.github.com>
2022-09-14 21:25:22 -0500

    Update and rename SophosFWcreate_objects_IPs.sh.sh to SophosFWcreate_objects_IPs.sh

--- SophosFWcreate_objects_IPs.sh
+++ SophosFWcreate_objects_IPs.sh
@@ -6,7 +6,7 @@
 # After configurying an API connection on your Firewall (see the following link) https://docs.sophos.com/nsg/sophos-firewall/18.5/Help/en-us/webhelp/onlinehelp/AdministratorHelp/BackupAndFirmware/API/APIUsingAPI/index.html and having downloaded (for example from https://github.com/firehol/blocklist-ipsets.git) or built by yourself an IP list, it is possible to create massively many single IP objects on Sophos Firewall.
 
 # Syntax
-# ./update_IPs_and_objects.sh $file_name $object_name_to_create $password
+# ./SophosFWcreate_objects_IPs.sh $file_name $object_name_to_create $password
 
 # Where
 # $file_name = the name of the file where IP addresses you want to add are
