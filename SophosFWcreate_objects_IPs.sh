69d01b8636300b25f3529aa198fcc5f02f9d3ec7
waltervalderrama <64512269+waltervalderrama@users.noreply.github.com>
2022-09-16 15:32:11 -0500

    Update SophosFWcreate_objects_IPs.sh

--- SophosFWcreate_objects_IPs.sh
+++ SophosFWcreate_objects_IPs.sh
@@ -5,12 +5,13 @@ CUT=/usr/bin/cut
 SED=/usr/bin/sed
 CURL=/usr/bin/curl
 PATH=$(pwd)
-filename=$1
+filename=$2
 n=1
 while read line; do
 # reading each line
 IP=$(echo "$line" | $SED -E "/^$|^\s*#/d")
-$CURL -k "https://firewall.valderrama.local:4444/webconsole/APIController" -d 'reqxml=<Request><Login><Username>API_adm</Username><Password>'$3'</Password></Login><Set><IPHost><Name>'$2$n'</Name><IPFamily>IPv4</IPFamily><HostType>IP</HostType><IPAddress>'$IP'</IPAddress></IPHost></Set></Request>' -v
+#$CURL -k "https://firewall.valderrama.local:4444/webconsole/APIController" -d 'reqxml=<Request><Login><Username>API_adm</Username><Password>'$3'</Password></Login><Set><IPHost><Name>'$2$n'</Name><IPFamily>IPv4</IPFamily><HostType>IP</HostType><IPAddress>'$IP'</IPAddress></IPHost></Set></Request>' -v
+$CURL -k "https://"$1":4444/webconsole/APIController" -d 'reqxml=<Request><Login><Username>'$3'</Username><Password>'$4'</Password></Login><Set><IPHost><Name>'$2$n'</Name><IPFamily>IPv4</IPFamily><HostType>IP</HostType><IPAddress>'$IP'</IPAddress></IPHost></Set></Request>' -v
 echo $IP
 n=$((n+1))
 done < $filename
